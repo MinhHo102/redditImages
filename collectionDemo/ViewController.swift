e6875f9e2e29d58daf8ae8c895526f3b78a89e81
minh ho <mho23@wirelessprv-10-192-71-228.near.illinois.edu>
2017-09-27 19:25:05 -0500

    Initial Commit

---
+++ collectionDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  collectionDemo
+//
+//  Created by minh ho on 9/27/17.
+//  Copyright © 2017 minh ho. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
